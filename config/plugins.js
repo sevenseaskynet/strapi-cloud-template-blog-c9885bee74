ac13aadc1a6e921e9864ea24a3f8baf6bfe38181
sevenseaskynet <ashiyanaapna2@gmail.com>
2025-05-23 11:35:45 +0530

    changes in package json
    changes in package json

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+module.exports = ({env}) => (
+    {
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET', '5c678d939d175ad51f084ac7b993d28f9799a75daff2c6a5646a4342f3d51b2c'),
+    },
+  },
+}
+);
